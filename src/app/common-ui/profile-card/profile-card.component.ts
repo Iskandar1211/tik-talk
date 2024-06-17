5477cc33c78d48df87dd4ed9592508c2b0368a01
DESKTOP-7K7V7QV\Lenovo <alexawp121109@gmail.com>
2024-06-17 14:16:25 +0500

    worked in showing profile data and binding, worked bor directive @for

--- src/app/common-ui/profile-card/profile-card.component.ts
+++ src/app/common-ui/profile-card/profile-card.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Profile } from '../../data/services/interfaces/profile.interface';
 
 @Component({
   selector: 'app-profile-card',
   standalone: true,
   imports: [],
   templateUrl: './profile-card.component.html',
-  styleUrl: './profile-card.component.scss'
+  styleUrl: './profile-card.component.scss',
 })
 export class ProfileCardComponent {
-
+  imgUrl = 'https://icherniakov.ru/yt-course/';
+  @Input() profile?: Profile;
 }
