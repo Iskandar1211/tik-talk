18f5f0b27c3bc04d2a9a6dde0552dfadc9d43a8a
DESKTOP-7K7V7QV\Lenovo <alexawp121109@gmail.com>
2024-06-17 13:32:42 +0500

    created profile-card component and styled, added icons directory in assets

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProfileCardComponent } from "./common-ui/profile-card/profile-card.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [RouterOutlet, ProfileCardComponent]
 })
 export class AppComponent {
   title = 'tik-talk';
---
+++ src/app/common-ui/profile-card/profile-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './profile-card.component.html',
+  styleUrl: './profile-card.component.scss'
+})
+export class ProfileCardComponent {
+
+}
