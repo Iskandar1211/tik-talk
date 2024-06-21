4887e78ebab561c8d44559b745c60c037cf559f8
DESKTOP-7K7V7QV\Lenovo <alexawp121109@gmail.com>
2024-06-21 14:53:32 +0500

    worked by @for in sidebar

--- src/app/common-ui/sidebar/sidebar.component.ts
+++ src/app/common-ui/sidebar/sidebar.component.ts
@@ -1,13 +1,35 @@
 import { Component } from '@angular/core';
 import { SvgIconComponent } from '../svg-icon/svg-icon.component';
 
+type MenuItems = {
+  label: string;
+  icon: string;
+  link: string;
+};
+
 @Component({
   selector: 'app-sidebar',
   standalone: true,
   imports: [SvgIconComponent],
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.scss'
+  styleUrl: './sidebar.component.scss',
 })
 export class SidebarComponent {
-
+  menuItems: MenuItems[] = [
+    {
+      label: 'Моя страница',
+      icon: 'home',
+      link: '',
+    },
+    {
+      label: 'Чаты',
+      icon: 'chat',
+      link: '/chat',
+    },
+    {
+      label: 'Поиск',
+      icon: 'search',
+      link: '/search',
+    },
+  ];
 }
