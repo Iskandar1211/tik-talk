f1c2c79e18991d92d3c1ea3b35ccc67bd1724850
DESKTOP-7K7V7QV\Lenovo <alexawp121109@gmail.com>
2024-07-03 21:57:42 +0500

    made the header reusable

--- src/app/common-ui/profile-header/profile-header.component.ts
+++ src/app/common-ui/profile-header/profile-header.component.ts
@@ -1,16 +1,18 @@
-import { Component, input } from '@angular/core';
+import {Component, Input, input} from '@angular/core';
 import { Profile } from '../../data/services/interfaces/profile.interface';
 import { ImgUrlPipe } from '../../helpers/pipes/img-url.pipe';
+import {NgClass} from "@angular/common";
 
 @Component({
   selector: 'app-profile-header',
   standalone: true,
   templateUrl: './profile-header.component.html',
   styleUrl: './profile-header.component.scss',
-  imports: [ImgUrlPipe],
+  imports: [ImgUrlPipe, NgClass],
 })
 export class ProfileHeaderComponent {
   profile = input<Profile>();
+  @Input() headerType: "profile" | 'profile-settings' = 'profile-settings'
 
   ngOnInit() {
     console.log('profile', this.profile());
