06decb2002741d9b9aae605cff18e94ca6b7e44f
DESKTOP-7K7V7QV\Lenovo <alexawp121109@gmail.com>
2024-07-04 20:42:09 +0500

    finished profile-page right panel

--- src/app/data/services/profile.service.ts
+++ src/app/data/services/profile.service.ts
@@ -26,11 +26,11 @@ export class ProfileService {
     return this.http.get<Profile>(`${this.baseApiUrl}account/${id}`);
   }
 
-  getSubscribersShortList() {
+  getSubscribersShortList(subsAmount:number = 3) {
     return this.http
       .get<Pagable<Profile>>(
         `${this.baseApiUrl}account/subscribers/?page=1&size=50`
       )
-      .pipe(map((res) => res.items.slice(0, 3)));
+      .pipe(map((res) => res.items.slice(0, subsAmount)));
   }
 }
--- src/app/pages/profile-page/profile-page.component.ts
+++ src/app/pages/profile-page/profile-page.component.ts
@@ -1,6 +1,6 @@
 import {AsyncPipe, NgStyle} from '@angular/common';
 import {Component, inject} from '@angular/core';
-import {ActivatedRoute, RouterLink, Router} from '@angular/router';
+import {ActivatedRoute, RouterLink, Router, RouterLinkActive} from '@angular/router';
 import {switchMap} from 'rxjs';
 import {ProfileHeaderComponent} from '../../common-ui/profile-header/profile-header.component';
 import {ProfileService} from '../../data/services/profile.service';
@@ -8,20 +8,21 @@ import {toObservable} from '../../helpers/toObservable';
 import {ImgUrlPipe} from "../../helpers/pipes/img-url.pipe";
 import {SvgIconComponent} from "../../common-ui/svg-icon/svg-icon.component";
 import {ReactiveFormsModule} from "@angular/forms";
+import {SubscriberCardComponent} from "../../common-ui/sidebar/subscriber-card/subscriber-card.component";
 
 @Component({
   selector: 'app-profile-page',
   standalone: true,
   templateUrl: './profile-page.component.html',
   styleUrl: './profile-page.component.scss',
-  imports: [ProfileHeaderComponent, AsyncPipe, NgStyle, ImgUrlPipe, SvgIconComponent, ReactiveFormsModule, RouterLink]
+  imports: [ProfileHeaderComponent, AsyncPipe, NgStyle, ImgUrlPipe, SvgIconComponent, ReactiveFormsModule, RouterLink, RouterLinkActive, SubscriberCardComponent]
 })
 export class ProfilePageComponent {
   profileService = inject(ProfileService);
   route = inject(ActivatedRoute);
   router = inject(Router)
   me$ = toObservable(this.profileService.me);
-
+  subscribers$ = this.profileService.getSubscribersShortList(5);
 
 
   profile$ = this.route.params.pipe(
