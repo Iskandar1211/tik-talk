3802f693c63c6c5ea9a8459f69a7889bd96f947f
DESKTOP-7K7V7QV\Lenovo <alexawp121109@gmail.com>
2024-07-04 19:41:37 +0500

    finished profile header styles

--- src/app/pages/profile-page/profile-page.component.ts
+++ src/app/pages/profile-page/profile-page.component.ts
@@ -1,35 +1,28 @@
 import {AsyncPipe, NgStyle} from '@angular/common';
 import {Component, inject} from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, RouterLink, Router} from '@angular/router';
 import {switchMap} from 'rxjs';
 import {ProfileHeaderComponent} from '../../common-ui/profile-header/profile-header.component';
 import {ProfileService} from '../../data/services/profile.service';
 import {toObservable} from '../../helpers/toObservable';
 import {ImgUrlPipe} from "../../helpers/pipes/img-url.pipe";
 import {SvgIconComponent} from "../../common-ui/svg-icon/svg-icon.component";
-import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {ReactiveFormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-profile-page',
   standalone: true,
   templateUrl: './profile-page.component.html',
   styleUrl: './profile-page.component.scss',
-  imports: [ProfileHeaderComponent, AsyncPipe, NgStyle, ImgUrlPipe, SvgIconComponent, ReactiveFormsModule]
+  imports: [ProfileHeaderComponent, AsyncPipe, NgStyle, ImgUrlPipe, SvgIconComponent, ReactiveFormsModule, RouterLink]
 })
 export class ProfilePageComponent {
-
-  profileForm = new FormGroup({
-    firstName: new FormControl('', Validators.required),
-    lastName: new FormControl('', Validators.required),
-    username: new FormControl('', Validators.required),
-    aboutMe: new FormControl('', Validators.required),
-
-  })
-
   profileService = inject(ProfileService);
   route = inject(ActivatedRoute);
+  router = inject(Router)
   me$ = toObservable(this.profileService.me);
-  stacks = ['JS', 'CSS', 'React', 'Next JS', 'Angular'];
+
+
 
   profile$ = this.route.params.pipe(
     switchMap(({id}) => {
